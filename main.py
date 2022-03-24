8e9876526618da8e85e82b40a0e36d1fc87fd840
omkarbhope <omkarbhope0309@gmail.com>
2022-03-24 20:29:03 +0530

    'v1'

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, Response, request,redirect,url_for
+from necklace_camera import NecklaceVideoCamera
+from tshirt_camera import TshirtVideoCamera
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    x = input('Enter video type: ')
+    if x == 'tshirt':
+        return Response(gen(TshirtVideoCamera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+    if x == 'necklace':
+        return Response(gen(NecklaceVideoCamera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8080, debug=True)
